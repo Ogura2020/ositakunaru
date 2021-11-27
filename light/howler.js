3bb0c22d4cd122147ff3fdf0ebcc19937d780c6b
Ogura2020 <cocoanapoli@gmail.com>
2021-11-28 00:50:48 +0900

    lightフォルダに追加しました

---
+++ light/howler.js
@@ -0,0 +1,16 @@
+const sound = new Howl({
+    src: ['audio/denki_suitti.mp3']
+});
+
+sound.play();
+
+// HTML側に`<button id="button1" />`がある前提で
+
+const click = document.getElementById('click').addEventListener('click', (e) => {
+    if (sound.playing()) {
+        sound.stop();
+        console.log('otoganaru')
+    } else {
+        sound.play();
+    }
+});
